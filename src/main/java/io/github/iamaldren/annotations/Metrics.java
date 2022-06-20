2d5adf643357a1291034d14da7092996fe2c39a9
Aldren <aldrenbobiles@gmail.com>
2022-06-20 22:50:50 +0800

    created annotations

---
+++ src/main/java/io/github/iamaldren/annotations/Metrics.java
@@ -0,0 +1,16 @@
+package io.github.iamaldren.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Metrics {
+
+    String timer() default "micrometer.handler.annotation.timer";
+    String gauge() default "micrometer.handler.annotation.gauge";
+    String count() default "micrometer.handler.annotation.count";
+
+}
